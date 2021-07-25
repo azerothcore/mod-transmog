dfd48b4c13d88ec98f2c8f078e7faaf1b24359d7
Winfidonarleyan <dowlandtop@yandex.com>
2021-07-25 20:26:41 +0700

    feat(Cmake): add support new script loader api

---
+++ src/transmog_loader.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ * Copyright (C) 2021+ WarheadCore <https://github.com/WarheadCore>
+ */
+
+#ifndef _TRANSMOG_LOADER_H_
+#define _TRANSMOG_LOADER_H_
+
+// From SC
+void AddSC_Transmog();
+
+// Add all
+void Addmod_transmogScripts()
+{
+    AddSC_Transmog();
+}
+
+#endif /* _TRANSMOG_LOADER_H_ */
