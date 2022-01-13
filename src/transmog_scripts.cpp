66b4d3317294ec7c1dbebf1fa575fe3bf1cc431b
Francesco Borzì <borzifrancesco@gmail.com>
2022-01-13 15:20:08 +0100

    fix(build): add missing override

--- src/transmog_scripts.cpp
+++ src/transmog_scripts.cpp
@@ -72,7 +72,7 @@ public:
         return true;
     }
 
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action)
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action) override
     {
         player->PlayerTalkClass->ClearMenus();
         WorldSession* session = player->GetSession();
