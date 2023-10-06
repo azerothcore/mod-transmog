d17fd83cc4df1a5b4ef5b46ae363a7743e6d0599
Paul <cn.paullee@gmail.com>
2023-10-06 02:45:04 +0000

    Add support for localized item names

--- src/transmog_scripts.cpp
+++ src/transmog_scripts.cpp
@@ -878,6 +878,12 @@ private:
         uint32 itemId = itemTemplate->ItemId;
         uint32 accountId = player->GetSession()->GetAccountId();
         std::string itemName = itemTemplate -> Name1;
+
+        // get locale item name
+        int loc_idex = player->GetSession()->GetSessionDbLocaleIndex();
+        if (ItemLocale const* il = sObjectMgr->GetItemLocale(itemId))
+            ObjectMgr::GetLocaleString(il->Name, loc_idex, itemName);
+
         std::stringstream tempStream;
         tempStream << std::hex << ItemQualityColors[itemTemplate->Quality];
         std::string itemQuality = tempStream.str();
