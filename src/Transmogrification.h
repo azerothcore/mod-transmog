2c928ec67c8def7bfc03376506aa6fb0fc1d0ecb
Andrew <47818697+Nyeriah@users.noreply.github.com>
2024-09-07 08:08:42 -0300

    refactor: Move subscriptions to its own setting (#176)

--- src/Transmogrification.h
+++ src/Transmogrification.h
@@ -29,7 +29,9 @@ enum TransmogSettings
 {
     SETTING_HIDE_TRANSMOG             = 0,
     SETTING_RETROACTIVE_CHECK         = 1,
-    SETTING_TRANSMOG_MEMBERSHIP_LEVEL = 2
+
+    // Subscriptions
+    SETTING_TRANSMOG_MEMBERSHIP_LEVEL = 0
 };
 
 enum MixedWeaponSettings
@@ -271,7 +273,7 @@ public:
     // Transmog Plus
     bool IsTransmogPlusEnabled;
     [[nodiscard]] bool IsPlusFeatureEligible(ObjectGuid const& playerGuid, uint32 feature) const;
-    [[nodiscard]] uint32 GetPlayerMembershipLevel(Player* player) const { return player->GetPlayerSetting("mod-transmog", SETTING_TRANSMOG_MEMBERSHIP_LEVEL).value; };
+    [[nodiscard]] uint32 GetPlayerMembershipLevel(Player* player) const { return player->GetPlayerSetting("acore_cms_subscriptions", SETTING_TRANSMOG_MEMBERSHIP_LEVEL).value; };
     [[nodiscard]] bool IgnoreLevelRequirement(ObjectGuid const& playerGuid) const { return IgnoreReqLevel || IsPlusFeatureEligible(playerGuid, PLUS_FEATURE_SKIP_LEVEL_REQ); }
 
     uint32 PetSpellId;
