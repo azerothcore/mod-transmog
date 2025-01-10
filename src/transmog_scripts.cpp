434d2957ea05f6a16a214de383735eeb3fc846ec
Andrew <47818697+Nyeriah@users.noreply.github.com>
2025-01-10 13:46:25 -0300

    feat: Implement transmog interface command (#188)
    * feat: Implement transmog interface command
    
    * Update transmog_scripts.cpp

--- src/transmog_scripts.cpp
+++ src/transmog_scripts.cpp
@@ -485,7 +485,7 @@ public:
                 }
             }
 
-            return sTransmogrification->IsEnabled() && (target && !target->GetPlayerSetting("mod-transmog", SETTING_HIDE_TRANSMOG).value);
+            return sTransmogrification->IsEnabled() && (target && !target->GetPlayerSetting("mod-transmog", SETTING_HIDE_TRANSMOG).IsEnabled());
         }
     };
 
@@ -538,13 +538,20 @@ public:
         switch (sender)
         {
             case EQUIPMENT_SLOT_END: // Show items you can use
+            {
                 sT->selectionCache[player->GetGUID()] = action;
 
+                bool useVendorInterface = player->GetPlayerSetting("mod-transmog", SETTING_VENDOR_INTERFACE).IsEnabled();
+
                 if (sT->GetUseVendorInterface())
-                    ShowTransmogItemsInFakeVendor(player, creature, action);
-                else
+                    useVendorInterface ? ShowTransmogItemsInFakeVendor(player, creature, action) :
                     ShowTransmogItemsInGossipMenu(player, creature, action, sender);
+                else
+                    useVendorInterface ? ShowTransmogItemsInGossipMenu(player, creature, action, sender) :
+                    ShowTransmogItemsInFakeVendor(player, creature, action);
+
                 break;
+            }
             case EQUIPMENT_SLOT_END + 1: // Main menu
                 OnGossipHello(player, creature);
                 break;
