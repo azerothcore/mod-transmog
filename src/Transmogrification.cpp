01c40bcec9d9a4ca8ce3556ffaf52bb2c997c861
Andrew <47818697+Nyeriah@users.noreply.github.com>
2025-12-24 09:19:23 -0300

    feat: move collections away from worldserver config reload (#219)

--- src/Transmogrification.cpp
+++ src/Transmogrification.cpp
@@ -1247,6 +1247,29 @@ bool Transmogrification::IsPlusFeatureEligible(ObjectGuid const &playerGuid, uin
     return false;
 }
 
+void Transmogrification::LoadCollections()
+{
+    if (sTransmogrification->GetUseCollectionSystem())
+    {
+        LOG_INFO("module", "Loading transmog appearance collection cache....");
+        uint32 collectedAppearanceCount = 0;
+        QueryResult result = CharacterDatabase.Query("SELECT account_id, item_template_id FROM custom_unlocked_appearances");
+        if (result)
+        {
+            do
+            {
+                uint32 accountId = (*result)[0].Get<uint32>();
+                uint32 itemId = (*result)[1].Get<uint32>();
+                if (sTransmogrification->AddCollectedAppearance(accountId, itemId))
+                    collectedAppearanceCount++;
+
+            } while (result->NextRow());
+        }
+
+        LOG_INFO("module", "Loaded {} collected appearances into cache", collectedAppearanceCount);
+    }
+}
+
 bool Transmogrification::GetEnableTransmogInfo() const
 {
     return EnableTransmogInfo;
